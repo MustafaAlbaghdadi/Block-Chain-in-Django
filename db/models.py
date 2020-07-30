47849f2968517452efe5119f37dd5c18847cdcde
Mustafa Albaghdadi <Redarrowd@gmail.com>
2020-07-31 00:48:13 +0300

    Add farmer factory distrbuted market account

--- db/models.py
+++ db/models.py
@@ -25,10 +25,34 @@ class Choice(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    expire_date = models.DateTimeField('date')
     count = models.IntegerField(default=1)
     ownerID  = models.IntegerField(default=0)
+    farmerID  = models.IntegerField(default=0)
+    farm_temp = models.CharField(max_length=200)
+    farm_humidity = models.CharField(max_length=200)
+    compost_type = models.CharField(max_length=200)
+    seed_type = models.CharField(max_length=200)
+    unite_type = models.CharField(max_length=200)
+    date_of_harvest = models.DateTimeField('date_of_harvest')
+    farm_location = models.CharField(max_length=200)
 
+    #Factory
+    factoryID  = models.IntegerField(default=0, blank=True, null=True)
+    canning_date = models.DateTimeField('date', blank=True, null=True)
+    processing_date = models.DateTimeField('date', blank=True, null=True)
+    expire_date = models.DateTimeField('date', blank=True, null=True)
+    factory_temp = models.CharField(max_length=200, blank=True, null=True)
+    factory_humidity = models.CharField(max_length=200, blank=True, null=True)
+    #destrbutor
+    distrabuterID  = models.IntegerField(default=0, blank=True, null=True)
+    entry_store_date = models.DateTimeField('date', blank=True, null=True)
+    destrbutor_temp = models.CharField(max_length=200, blank=True, null=True)
+    destrbutor_humidity = models.CharField(max_length=200, blank=True, null=True)
+    #markting
+    marktingID  = models.IntegerField(default=0, blank=True, null=True)
+    entry_market_date = models.DateTimeField('date', blank=True, null=True)
+    market_temp = models.CharField(max_length=200, blank=True, null=True)
+    market_humidity = models.CharField(max_length=200, blank=True, null=True)
     def __str__(self):
         return self.Name
 
--- db/views.py
+++ db/views.py
@@ -2,10 +2,11 @@ from datetime import datetime
 from django.utils.dateparse import parse_date
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render,redirect
-from db.models import Question, Product,Contract
+from django.shortcuts import render, redirect
+from db.models import Question, Product, Contract
 from django.contrib.auth.models import User
 import hashlib
+
 
 def products(request):
     if request.user.is_authenticated:
@@ -16,6 +17,7 @@ def products(request):
     else:
         return redirect("/admin")
 
+
 def AllProduct(request):
     if request.user.is_authenticated:
         productList = Product.objects.all()
@@ -23,6 +25,8 @@ def AllProduct(request):
         return render(request, 'db/AllProduct.html', context)
     else:
         return redirect("/admin")
+
+
 def contracts(request):
     if request.user.is_authenticated:
         contracts = Contract.objects.all()
@@ -36,15 +40,24 @@ def contracts(request):
     else:
         return redirect("/admin")
 
+
 def addProduct(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and str(request.user.groups.all().first()) == 'Farmer':
         if request.method == "POST":
-            name = request.POST["Name"]
-            count = request.POST["Count"]
-            exireDate = parse_date(request.POST["ExireDate"])
-            ownerId=request.user.id
-            pubDate = datetime.now().date()
-            product = Product(name=name , count=count,expire_date=exireDate,ownerID=ownerId,pub_date=pubDate)
+
+            product = Product(name=request.POST["Name"],
+                              count=request.POST["Count"],
+                              ownerID=request.user.id,
+                              pub_date=datetime.now().date(),
+                              farmerID=request.user.id,
+                              farm_temp=request.POST["farm_temp"],
+                              farm_humidity=request.POST["farm_humidity"],
+                              compost_type=request.POST["compost_type"],
+                              seed_type=request.POST["seed_type"],
+                              unite_type=request.POST["unite_type"],
+                              date_of_harvest=request.POST["date_of_harvest"],
+                              farm_location=request.POST["farm_location"],
+                              )
             product.save()
             return redirect("/products")
         else:
@@ -53,8 +66,9 @@ def addProduct(request):
     else:
         return redirect("/admin")
 
+
 class ContractModel:
-    def __init__(self, Id, seller,buyer, info, date,prehash, currentHash):
+    def __init__(self, Id, seller, buyer, info, date, prehash, currentHash):
         self.Id = Id
         self.seller = seller
         self.buyer = buyer
@@ -62,54 +76,131 @@ class ContractModel:
         self.date = date
         self.prehash = prehash
         self.currentHash = currentHash
-def productDetails(request,productd_id):
+
+
+def productDetails(request, productd_id):
     product = get_object_or_404(Product, id=productd_id)
-    contracts = Contract.objects.filter(product_id = productd_id)
+    contracts = Contract.objects.filter(product_id=productd_id)
     cons = []
     for con in contracts:
-        seller = User.objects.filter(id = con.sellerID).first()
-        buyer = User.objects.filter(id = con.buyerID).first()
-        cons.append(ContractModel(con.id,seller,buyer,con.info,con.date,'',''))
+        seller = User.objects.filter(id=con.sellerID).first()
+        buyer = User.objects.filter(id=con.buyerID).first()
+        cons.append(ContractModel(con.id, seller, buyer, con.info, con.date, '', ''))
     ownerId = request.user.id
     Sellable = 'False'
     if product.ownerID == ownerId:
         Sellable = 'True'
-    return render(request, 'db/product.html', {'product': product , "contracts":cons,"sellable":Sellable})
+    return render(request, 'db/product.html', {'product': product, "contracts": cons, "sellable": Sellable,
+                                               "farmer": User.objects.filter(id=product.farmerID).first(),
+                                               "factory": User.objects.filter(id=product.factoryID).first(),
+                                               "distrabuter": User.objects.filter(id=product.distrabuterID).first(),
+                                               "markting": User.objects.filter(id=product.marktingID).first(),
+                                               })
 
-def sell(request,productd_id):
+
+def sell(request, productd_id):
     if request.user.is_authenticated:
-            users = User.objects.all()
-            product = get_object_or_404(Product, id=productd_id)
-            ownerId = request.user.id
-            if(product.ownerID != ownerId):
-                return redirect("/products")
-            return render(request, 'db/sell.html', {'product': product ,"users":users })
+        users = User.objects.all()
+        product = get_object_or_404(Product, id=productd_id)
+        state = 0
+        if product.factoryID == 0:
+            users = User.objects.filter(groups__name__in=['Factory']).all()
+            state = 1
+        if product.distrabuterID == 0:
+            users = User.objects.filter(groups__name__in=['Distrabuter']).all()
+            state = 2
+        if product.marktingID == 0:
+            users = User.objects.filter(groups__name__in=['marketer']).all()
+            state = 3
+        ownerId = request.user.id
+        if (product.ownerID != ownerId):
+            return redirect("/products")
+        return render(request, 'db/sell.html', {'product': product, "users": users, "state": state})
     else:
         return redirect("/admin")
 
+
 def postSell(request):
-    if  request.method == "POST":
+    if request.method == "POST":
         if request.user.is_authenticated:
             ownerId = request.user.id
             newOnwer = request.POST["buyer"]
             productID = request.POST["productID"]
             product = get_object_or_404(Product, id=productID)
             des = request.POST["des"]
-            data =str(ownerId) + str(newOnwer) + str(productID) +des
+            data = str(ownerId) + str(newOnwer) + str(productID) + des
             hash_object = hashlib.md5(bytes(data, encoding='utf-8'))
-            hash =hash_object.hexdigest()
-            prehash ='',
-            product.ownerID = newOnwer
-            if Contract.objects.count()>0:
-                prehash = Contract.objects.reverse()[Contract.objects.count()-1].strHash
-            contract = Contract(info = des,strHash = hash ,sellerID=ownerId,buyerID=newOnwer,date= datetime.now().date()
+            hash = hash_object.hexdigest()
+            prehash = '',
+            if Contract.objects.count() > 0:
+                prehash = Contract.objects.reverse()[Contract.objects.count() - 1].strHash
+            contract = Contract(info=des, strHash=hash, sellerID=ownerId, buyerID=newOnwer, date=datetime.now().date()
                                 , product_id=productID, prevHash=prehash)
+
+            if request.POST["state"] == '1':
+                product.ownerID = newOnwer
+                product.canning_date = request.POST["canning_date"]
+                product.processing_date = request.POST["processing_date"]
+                product.expire_date = request.POST["expire_date"]
+                product.factory_temp = request.POST["factory_temp"]
+                product.factory_humidity = request.POST["factory_humidity"]
+                product.factoryID = newOnwer
+            if request.POST["state"] == '2':
+                product.ownerID = newOnwer
+                product.distrabuterID = newOnwer
+                product.destrbutor_temp = request.POST["destrbutor_temp"]
+                product.destrbutor_humidity = request.POST["destrbutor_humidity"]
+                product.entry_store_date = request.POST["entry_store_date"]
+                product.unite_type = request.POST["unite_type"]
+                product.count = request.POST["Count"]
+            if request.POST["state"] == '3':
+                if int(request.POST["Count"]) < product.count:
+                    p = Product(
+                        name=product.name,
+                        pub_date=product.pub_date,
+                        ownerID=newOnwer,
+                        farmerID=product.farmerID,
+                        farm_temp=product.farm_temp,
+                        farm_humidity=product.farm_humidity,
+                        compost_type=product.compost_type,
+                        seed_type=product.seed_type,
+                        date_of_harvest=product.date_of_harvest,
+                        farm_location=product.farm_location,
+                        factoryID=product.factoryID,
+                        canning_date=product.canning_date,
+                        processing_date=product.processing_date,
+                        expire_date=product.expire_date,
+                        factory_temp=product.factory_temp,
+                        factory_humidity=product.factory_humidity,
+                        distrabuterID=product.distrabuterID,
+                        entry_store_date=product.entry_store_date,
+                        destrbutor_temp=product.destrbutor_temp,
+                        destrbutor_humidity=product.destrbutor_humidity,
+                        market_temp=request.POST["market_temp"],
+                        market_humidity=request.POST["market_humidity"],
+                        unite_type=request.POST["unite_type"],
+                        entry_market_date=request.POST["entry_market_date"],
+                        count=request.POST["Count"]
+
+                    )
+                    product.count = int(product.count) - int(p.count)
+                    p.save()
+                else:
+                    product.ownerID = newOnwer
+                    product.marktingID = newOnwer
+                    product.market_temp = request.POST["market_temp"]
+                    product.market_humidity = request.POST["market_humidity"]
+                    product.entry_market_date = request.POST["entry_market_date"]
+                    product.unite_type = request.POST["unite_type"]
+                    product.count = request.POST["Count"]
+
             product.save()
             contract.save()
             return redirect("/products/")
         else:
             return redirect("/admin")
 
-def index(request):
-    return render(request, 'db/index.html')
 
+def index(request):
+    group = request.user.groups.all().first()
+    return render(request, 'db/index.html', {'mygroup': group})
